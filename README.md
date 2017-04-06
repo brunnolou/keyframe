25e4d3fbf3a51e7d36cf1562c35b893116327462
Bruno Lourenco <brunnolou@gmail.com>
2017-04-06 18:19:57 +0100

    Add example

--- README.md
+++ README.md
@@ -6,7 +6,35 @@
 `npm install Keyframe`
 
 ## Usage
-`@TODO`
+```js
+import keyframe from '../';
+
+...
+
+let i = 0;
+let progress = 0;
+const duration = 2 * 60;
+
+function update() {
+  progress = i / duration;
+
+  // From (0% -> 50%) move the div left 100px
+  // then from (50% -> 100%) move the div down 100px.
+  keyframe({
+    // d is the duration between 0% -> 50%.
+    0: (d) => moveTo(d * 100, 0),
+
+    // d is the duration between 50% -> 100%.
+    50: (d) => moveTo(100, d * 100),
+  }, progress);
+
+  i++;
+
+  requestAnimationFrame(update);
+}
+
+requestAnimationFrame(update);
+```
 
 ## Development
 `npm install`
--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
 import keyframe from '../';
 
-//@TODO demo.
+// Create a div element.
+const div = document.createElement('div');
+div.innerHTML = 'DEMO';
+document.documentElement.appendChild(div);
 
-// Request Animation Frame logic.
+// Utility to move the div
+moveTo = (x, y) => {
+  div.style.transform = `translate(${x}px, ${y}px)`;
+};
+
+let i = 0;
+let progress = 0;
+const duration = 2 * 60;
+
 function update() {
+  progress = i / duration;
+
+  // From (0% -> 50%) move the div left 100px
+  // then from (50% -> 100%) move the div down 100px.
+  keyframe({
+    // d is the duration between 0% -> 50%.
+    0: (d) => moveTo(d * 100, 0),
+
+    // d is the duration between 50% -> 100%.
+    50: (d) => moveTo(100, d * 100),
+  }, progress);
+
+  i++;
+
   requestAnimationFrame(update);
 }
 
 requestAnimationFrame(update);
-
